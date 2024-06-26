521587b6289d54268e86428b8ad1d958fd20bd3b
Léo James <l_james8@hotmail.com>
2024-06-26 23:36:29 +0200

    Prefer Uint8Array to Buffer for now

--- src/hormann/utils.ts
+++ src/hormann/utils.ts
@@ -1,9 +1,9 @@
 import { CRC } from "crc-full";
 
 const crc = new CRC("CRC8", 8, 0x07, 0xf3, 0x00, false, false);
-export function computeCRC8(bytes: number[] | Buffer): number {
+export function computeCRC8(bytes: number[] | Uint8Array): number {
   if (bytes.length == 0) {
     throw new Error('Bytes cannot be empty');
   }
-  return crc.compute(bytes);
+  return crc.compute(bytes as Buffer);
 }
