a0162103ab1e3821bcfbfa3b477f5e7c030692f4
Léo James <l_james8@hotmail.com>
2024-10-28 21:10:54 +0100

    Added HCPClient interface and MockHCPClient class to test without the hardware
    still some doc to write and exports to improve

--- src/garagedoor.ts
+++ src/garagedoor.ts
@@ -3,14 +3,14 @@ import { EventEmitter } from "events";
 import Debug, { Debugger } from "debug";
 
 import {
+  HCPClient,
   SerialHCPClient,
   SerialOptions,
   STATUS_RESPONSE_BYTE0_BITFIELD,
   BROADCAST_STATUS_BYTE0_BITFIELD,
   DIRECTION,
-  HCPClient,
 } from "./serialHCPClient";
-import { PacketFilterParams } from "./parser";
+import { HCPPacket, PacketFilterParams } from "./parser";
 
 export { SerialOptions, PacketFilterParams };
 
@@ -27,6 +27,11 @@ export enum TargetDoorState {
   OPEN,
   CLOSED,
   VENTING = 5,
+}
+
+interface GarageState {
+  door: CurrentDoorState;
+  light: boolean;
 }
 
 abstract class GarageDoorOpener extends EventEmitter {
@@ -54,7 +59,7 @@ export class HormannGarageDoorOpener extends GarageDoorOpener {
   broadcastStatus: Uint8Array;
 
   constructor(
-    name: string,
+    name: string = "Hörmann Garage Door",
     public hcpClient: HCPClient,
   ) {
     super(name);
@@ -82,9 +87,7 @@ export class HormannGarageDoorOpener extends GarageDoorOpener {
     }
   }
 
-  static broadcastToCurrentState(
-    status: Uint8Array,
-  ): { door: CurrentDoorState; light: boolean } | Error {
+  static broadcastToCurrentState(status: Uint8Array): GarageState | Error {
     const bitField = SerialHCPClient.extractBitfield(status[0]);
     if (bitField[BROADCAST_STATUS_BYTE0_BITFIELD.ERROR_ACTIVE] === true) {
       return new Error("Error active");
@@ -199,4 +202,154 @@ export function createHormannGarageDoorOpener(
     name,
     new SerialHCPClient({ path, ...rest }, { packetTimeout, filterBreaks, filterMaxLength }),
   );
+}
+
+// TODO: write some doc
+export class MockHCPClient extends EventEmitter implements HCPClient {
+  // keeping track of the parent garageState can be useful
+  public mockState: GarageState = { door: CurrentDoorState.CLOSED, light: false };
+
+  constructor(
+    public pushCommandMock: (
+      flags: STATUS_RESPONSE_BYTE0_BITFIELD[],
+      emergencyStop: boolean,
+    ) => Promise<HCPPacket> = () =>
+      new Promise((resolve) => {
+        // just a dummy packet return after 30ms
+        setTimeout(() => {
+          resolve(HCPPacket.fromData(0x80, 0x01, [0xff, 0xff]));
+        }, 30);
+      }),
+  ) {
+    super();
+  }
+
+  inferPushCommandMock(
+    flags: STATUS_RESPONSE_BYTE0_BITFIELD[],
+    emergencyStop: boolean,
+  ): Promise<HCPPacket> {
+    /** smarter pushCommandMock method that infers what to emit based on command and mockState */
+    let callback = null;
+    if (emergencyStop === true) {
+      callback = () => {
+        this.emitDoorState(CurrentDoorState.STOPPED);
+      };
+    } else {
+      // infer next state
+      const nextState = MockHCPClient.responseStatusToNextState(flags, this.mockState);
+      callback = () => {
+        this.emitGarageState(nextState);
+      };
+    }
+    return new Promise((resolve) => {
+      // TODO: parametrize timeout values
+      // confirm command received after 30ms
+      setTimeout(() => {
+        resolve(HCPPacket.fromData(0x80, 0x01, [0xff, 0xff]));
+        // emit next door state after 100ms
+        setTimeout(callback, 100);
+      }, 30);
+    });
+  }
+
+  pushCommand(
+    flags: STATUS_RESPONSE_BYTE0_BITFIELD[],
+    emergencyStop: boolean = false,
+  ): Promise<HCPPacket> {
+    return this.pushCommandMock(flags, emergencyStop);
+  }
+
+  static doorStateToBroadcastStatus(state: CurrentDoorState): Uint8Array {
+    const status = new Uint8Array(2);
+    // set arbitrary 2nd status byte value
+    status[1] = 0xff;
+    switch (state) {
+      case CurrentDoorState.CLOSED:
+        status[0] = 1 << BROADCAST_STATUS_BYTE0_BITFIELD.DOOR_CLOSED;
+        break;
+      case CurrentDoorState.OPEN:
+        status[0] = 1 << BROADCAST_STATUS_BYTE0_BITFIELD.DOOR_OPENED;
+        break;
+      case CurrentDoorState.CLOSING: {
+        status[0] = 1 << BROADCAST_STATUS_BYTE0_BITFIELD.DOOR_MOVING;
+        // set direction
+        status[0] |= (1 << BROADCAST_STATUS_BYTE0_BITFIELD.DOOR_DIRECTION) * DIRECTION.CLOSING;
+        break;
+      }
+      case CurrentDoorState.OPENING: {
+        status[0] = 1 << BROADCAST_STATUS_BYTE0_BITFIELD.DOOR_MOVING;
+        // set direction
+        status[0] |= (1 << BROADCAST_STATUS_BYTE0_BITFIELD.DOOR_DIRECTION) * DIRECTION.OPENING;
+        break;
+      }
+      case CurrentDoorState.VENTING:
+        status[0] = 1 << BROADCAST_STATUS_BYTE0_BITFIELD.DOOR_VENTING;
+        break;
+      case CurrentDoorState.STOPPED:
+        status[0] = 1 << BROADCAST_STATUS_BYTE0_BITFIELD.ERROR_ACTIVE;
+        break;
+    }
+    return status;
+  }
+
+  static lightStateToBroadcastStatus(state: boolean, initialState: number = 0x01): Uint8Array {
+    const status = new Uint8Array(2);
+    status[0] = initialState;
+    status[1] = 0xff;
+    if (state === true) {
+      // light on flag is set, else nothing is set
+      status[0] |= 1 << BROADCAST_STATUS_BYTE0_BITFIELD.LIGHT_RELAY_ON;
+    }
+    return status;
+  }
+
+  static responseStatusToNextState(
+    flags: STATUS_RESPONSE_BYTE0_BITFIELD[],
+    currentState: GarageState,
+  ): GarageState {
+    /** guess logical next state from input command and current state */
+    const nextState = currentState;
+    if (flags.includes(STATUS_RESPONSE_BYTE0_BITFIELD.TOGGLE_LIGHT)) {
+      nextState.light = !currentState.light;
+    }
+    if (flags.includes(STATUS_RESPONSE_BYTE0_BITFIELD.VENTING)) {
+      nextState.door = CurrentDoorState.VENTING;
+    } else if (flags.includes(STATUS_RESPONSE_BYTE0_BITFIELD.CLOSE)) {
+      nextState.door =
+        currentState.door === CurrentDoorState.CLOSED
+          ? CurrentDoorState.CLOSED
+          : CurrentDoorState.CLOSING;
+    } else if (flags.includes(STATUS_RESPONSE_BYTE0_BITFIELD.OPEN)) {
+      nextState.door =
+        currentState.door === CurrentDoorState.OPEN
+          ? CurrentDoorState.OPEN
+          : CurrentDoorState.OPENING;
+    }
+
+    return nextState;
+  }
+
+  emitGarageState(state: GarageState): boolean {
+    const nextState = MockHCPClient.lightStateToBroadcastStatus(
+      state.light,
+      MockHCPClient.doorStateToBroadcastStatus(state.door)[0],
+    );
+    const success = this.emit("data", nextState);
+    if (success === true) this.mockState = state;
+    return success;
+  }
+
+  emitDoorError(): boolean {
+    const status = new Uint8Array(2);
+    status[0] = 1 << BROADCAST_STATUS_BYTE0_BITFIELD.ERROR_ACTIVE;
+    return this.emit("data", status);
+  }
+
+  emitDoorState(state: CurrentDoorState): boolean {
+    return this.emit("data", MockHCPClient.doorStateToBroadcastStatus(state));
+  }
+
+  emitLightState(state: boolean, initialState?: number): boolean {
+    return this.emit("data", MockHCPClient.lightStateToBroadcastStatus(state, initialState));
+  }
 }
--- tests/garageDoor.test.ts
+++ tests/garageDoor.test.ts
@@ -1,5 +1,10 @@
-import { CurrentDoorState, HormannGarageDoorOpener, TargetDoorState } from "@src/garagedoor";
-import { SerialHCPClient } from "@src/serialHCPClient";
+import {
+  CurrentDoorState,
+  HormannGarageDoorOpener,
+  MockHCPClient,
+  TargetDoorState,
+} from "@src/garagedoor";
+import { SerialHCPClient, STATUS_RESPONSE_BYTE0_BITFIELD } from "@src/serialHCPClient";
 import { HCPPacket } from "@src/parser";
 jest.mock("serialport");
 
@@ -197,6 +202,194 @@ describe("garageDoor set state", () => {
       expect(garage.getLightOnState()).toBe(false);
       expect(loggerSpy).toHaveBeenLastCalledWith("Light On state already false");
       expect(mockPushCommand).not.toHaveBeenCalled();
+    });
+  });
+});
+
+describe("MockHCPClient tests", () => {
+  it("should mock the right broadcast statuses from given door states", () => {
+    expect(MockHCPClient.doorStateToBroadcastStatus(CurrentDoorState.CLOSED)).toEqual(
+      Uint8Array.from([0x01, 0xff]),
+    );
+    expect(MockHCPClient.doorStateToBroadcastStatus(CurrentDoorState.OPEN)).toEqual(
+      Uint8Array.from([0x02, 0xff]),
+    );
+    expect(MockHCPClient.doorStateToBroadcastStatus(CurrentDoorState.VENTING)).toEqual(
+      Uint8Array.from([0x80, 0xff]),
+    );
+    expect(MockHCPClient.doorStateToBroadcastStatus(CurrentDoorState.CLOSING)).toEqual(
+      Uint8Array.from([0x60, 0xff]),
+    );
+    expect(MockHCPClient.doorStateToBroadcastStatus(CurrentDoorState.OPENING)).toEqual(
+      Uint8Array.from([0x40, 0xff]),
+    );
+  });
+
+  it("should mock the right broadcast statuses from light states", () => {
+    expect(MockHCPClient.lightStateToBroadcastStatus(false)).toEqual(Uint8Array.from([0x01, 0xff]));
+    expect(MockHCPClient.lightStateToBroadcastStatus(true)).toEqual(Uint8Array.from([0x09, 0xff]));
+    expect(MockHCPClient.lightStateToBroadcastStatus(false, 0x02)).toEqual(
+      Uint8Array.from([0x02, 0xff]),
+    );
+  });
+
+  it("should infer the right following states from previous ones", () => {
+    // closed > opening
+    expect(
+      MockHCPClient.responseStatusToNextState([STATUS_RESPONSE_BYTE0_BITFIELD.OPEN], {
+        door: CurrentDoorState.CLOSED,
+        light: false,
+      }),
+    ).toEqual({door: CurrentDoorState.OPENING, light: false});
+    // opened > closing
+    expect(
+      MockHCPClient.responseStatusToNextState([STATUS_RESPONSE_BYTE0_BITFIELD.CLOSE], {
+        door: CurrentDoorState.OPEN,
+        light: false,
+      }),
+    ).toEqual({door: CurrentDoorState.CLOSING, light: false});
+    // already opened
+    expect(
+      MockHCPClient.responseStatusToNextState([STATUS_RESPONSE_BYTE0_BITFIELD.CLOSE], {
+        door: CurrentDoorState.CLOSED,
+        light: false,
+      }),
+    ).toEqual({door: CurrentDoorState.CLOSED, light: false});
+    // already closed
+    expect(
+      MockHCPClient.responseStatusToNextState([STATUS_RESPONSE_BYTE0_BITFIELD.OPEN], {
+        door: CurrentDoorState.OPEN,
+        light: false,
+      }),
+    ).toEqual({door: CurrentDoorState.OPEN, light: false});
+  });
+
+  describe("MockHCPClient instanciated tests", () => {
+    let mock: MockHCPClient;
+    let mockGarageDoor: HormannGarageDoorOpener;
+
+    beforeEach(() => {
+      mock = new MockHCPClient();
+      mockGarageDoor = new HormannGarageDoorOpener(undefined, mock);
+    });
+
+    it("should display the right name", () => {
+      expect(mockGarageDoor.name).toBe("Hörmann Garage Door");
+    });
+
+    it("should emit an error when requested", (done) => {
+      mockGarageDoor.once("error", (err) => {
+        expect(err).toEqual(new Error("Error active"));
+        done();
+      });
+      mock.emitDoorError();
+    });
+
+    it("should emit mocked door states", () => {
+      // state not set
+      expect(() => {
+        mockGarageDoor.getCurrentState();
+      }).toThrow();
+      mock.emitDoorState(CurrentDoorState.OPENING);
+      expect(mockGarageDoor.getCurrentState()).toBe(CurrentDoorState.OPENING);
+      mock.emitDoorState(CurrentDoorState.CLOSED);
+      expect(mockGarageDoor.getCurrentState()).toBe(CurrentDoorState.CLOSED);
+    });
+
+    it("should emit mocked light states", () => {
+      // state set (side-effect from previous test)
+      mock.emitLightState(false);
+      expect(mockGarageDoor.getLightOnState()).toBe(false);
+      mock.emitLightState(true);
+      expect(mockGarageDoor.getLightOnState()).toBe(true);
+    });
+
+    it("should emit mocked garage states", () => {
+      mock.emitGarageState({ door: CurrentDoorState.OPEN, light: true });
+      expect(mockGarageDoor.getCurrentState()).toBe(CurrentDoorState.OPEN);
+      expect(mockGarageDoor.getLightOnState()).toBe(true);
+    });
+
+    it("should mock pushing some commands to the hcp client", () => {
+      mock.emitDoorState(CurrentDoorState.CLOSED);
+      expect(() => {
+        mockGarageDoor.getTargetState();
+      }).toThrow();
+      return mockGarageDoor.setTargetState(TargetDoorState.OPEN).then(() => {
+        expect(mockGarageDoor.getTargetState()).toBe(TargetDoorState.OPEN);
+      });
+    });
+
+    it("should allow to infer some mock states", async () => {
+      // change push command callback to infer the next state automatically
+      mock.pushCommandMock = mock.inferPushCommandMock;
+
+      const handleDoorUpdate = (expectedValue: CurrentDoorState) =>
+        new Promise<void>((resolve) => {
+          mockGarageDoor.once("update_door", (state: CurrentDoorState) => {
+            expect(state).toBe(expectedValue);
+            resolve();
+          });
+        });
+      const handleLightUpdate = (expectedValue: boolean) =>
+        new Promise<void>((resolve) => {
+          mockGarageDoor.once("update_light", (state: boolean) => {
+            expect(state).toBe(expectedValue);
+            resolve();
+          });
+        });
+
+      const errorEvent = new Promise<void>((resolve) => {
+        mockGarageDoor.once("error", (err) => {
+          expect(err).toEqual(new Error("Error active"));
+          resolve();
+        });
+      });
+
+      // test closing
+      mock.emitGarageState({ door: CurrentDoorState.OPEN, light: false });
+      await mockGarageDoor
+        .setTargetState(TargetDoorState.CLOSED)
+        .then(() => {
+          expect(mockGarageDoor.getTargetState()).toBe(TargetDoorState.CLOSED);
+          // state not yet changed
+          expect(mockGarageDoor.getCurrentState()).toBe(CurrentDoorState.OPEN);
+        })
+        .then(() => handleDoorUpdate(CurrentDoorState.CLOSING));
+
+      // test opening
+      mock.emitGarageState({ door: CurrentDoorState.CLOSED, light: false });
+      await mockGarageDoor
+        .setTargetState(TargetDoorState.OPEN)
+        .then(() => {
+          expect(mockGarageDoor.getTargetState()).toBe(TargetDoorState.OPEN);
+          // state not yet changed
+          expect(mockGarageDoor.getCurrentState()).toBe(CurrentDoorState.CLOSED);
+        })
+        .then(() => handleDoorUpdate(CurrentDoorState.OPENING));
+
+      // test venting
+      mock.emitGarageState({ door: CurrentDoorState.OPEN, light: false });
+      await mockGarageDoor
+        .setTargetState(TargetDoorState.VENTING)
+        .then(() => {
+          expect(mockGarageDoor.getTargetState()).toBe(TargetDoorState.VENTING);
+          // state not yet changed
+          expect(mockGarageDoor.getCurrentState()).toBe(CurrentDoorState.OPEN);
+        })
+        .then(() => handleDoorUpdate(CurrentDoorState.VENTING));
+
+      // emergency stop emits an error, STOPPED not (yet) emitted (see onBroadcast)
+      await mock.pushCommand([], true).then(() => errorEvent);
+      // test lights on
+      mock.emitGarageState({ door: CurrentDoorState.VENTING, light: false });
+      await mockGarageDoor
+        .setLightOnState(true)
+        .then(() => {
+          // state not yet changed
+          expect(mockGarageDoor.getLightOnState()).toBe(false);
+        })
+        .then(() => handleLightUpdate(true));
     });
   });
 });
